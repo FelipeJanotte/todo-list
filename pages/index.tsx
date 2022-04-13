b491d9f1fb647592cb38a0c0d53dc44ef34c8bc0
Felipe Augusto Janotte <f.a.janotte@gmail.com>
2022-04-13 09:01:55 -0300

    Fixed: select last item of list

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,6 +20,8 @@ interface PostProps {
 const Home = ({ todos }: PostProps) => {
   const [description, setDescription] = useState("");
   const [visibilityModal, setVisibilityModal] = useState(false);
+  const [descriptionOld, setDescriptionOld] = useState("");
+  const [idToUpdate, setIdToUpdate] = useState(0);
 
   const deleteTodo = async (id: number) => {
     await fetch("/api/todo/", {
@@ -52,6 +54,8 @@ const Home = ({ todos }: PostProps) => {
                     onClick={() => {
                       setVisibilityModal(true);
                       setDescription("");
+                      setDescriptionOld(item.description);
+                      setIdToUpdate(item.id);
                     }}
                   >
                     <svg
@@ -101,12 +105,12 @@ const Home = ({ todos }: PostProps) => {
               </div>
               <div
                 style={{ display: visibilityModal ? "block" : "none" }}
-                className="modal h-screen w-screen absolute items-center justify-center top-0 bg-black bg-opacity-60 p-16"
+                className="modal h-screen w-screen fixed items-center justify-center top-0 bg-black bg-opacity-60 p-16"
               >
                 <div className="bg-gray-50 opacity-100 max-w-xl mx-auto p-4 sm:p-6 lg:p-8 rounded-2xl">
                   <h2 className="text-xl font-medium tracking-tight text-gray-900 sm:text-2xl">
                     <span className="block m-2">Old todo:</span>
-                    <span className="block text-green-600">{item.description}</span>
+                    <span className="block text-green-600">{descriptionOld}</span>
                     <span className="block m-2">New todo:</span>
                     <input
                       className="border-2 rounded-lg"
@@ -120,7 +124,7 @@ const Home = ({ todos }: PostProps) => {
                       <button
                         className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white duration-150 bg-green-600 hover:bg-green-700"
                         onClick={() => {
-                          updateTodo(item.id);
+                          updateTodo(idToUpdate);
                           setVisibilityModal(false);
                         }}
                       >
